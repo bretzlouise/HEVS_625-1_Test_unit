795d106d35060fbdf60ebf9757aad97c871ed574
Louise <bretzlouise@gmail.com>
2021-11-19 10:48:32 +0100

    Reference + names of method

--- C#/TestImageEdgeDetection/ImageEdgeDetectionTest.cs
+++ C#/TestImageEdgeDetection/ImageEdgeDetectionTest.cs
@@ -1,12 +1,40 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ImageEdgeDetection;
 
 namespace TestImageEdgeDetection
 {
     [TestClass]
-    public class UnitTest1
+    public class ImageEdgeDetectionTest
     {
+        [TestInitialize]
+        public void Init()
+        {
+        }
         [TestMethod]
-        public void TestMethod1()
+        public void NoneFilterTest()
+        {
+        }
+
+        [TestMethod]
+        public void RainbowFilterTest()
+        {
+        }
+
+        [TestMethod]
+        public void SwapFilterTest()
+        {
+        }
+        [TestMethod]
+        public void KirschEdgeDetectionTest()
+        {
+        }
+        [TestMethod]
+        public void Sobel3x3EdgeDetectionTest()
+        {
+        }
+
+        [TestMethod]
+        public void PrewittEdgeDetectionTest()
         {
         }
     }

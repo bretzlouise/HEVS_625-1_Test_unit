019e943f8bc4f3a8f9ba49bc36821ddbc76e97f0
Louise <bretzlouise@gmail.com>
2021-11-26 11:19:26 +0100

    save brainstorming

--- C#/TestImageEdgeDetection/ImageEdgeDetectionTest.cs
+++ C#/TestImageEdgeDetection/ImageEdgeDetectionTest.cs
@@ -1,5 +1,7 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using ImageEdgeDetection;
+using System.Drawing;
+using System.IO;
 
 namespace TestImageEdgeDetection
 {
@@ -27,6 +29,43 @@ namespace TestImageEdgeDetection
         [TestMethod]
         public void KirschEdgeDetectionTest()
         {
+
+            //Arrange
+            //Bitmap initialPicture = new Bitmap(@"..\\..\\..\\Pictures\\forrest.png");
+            //Bitmap initialPicture = new Bitmap(@"Pictures\forrest.png");
+            //Bitmap initialPicture = new Bitmap(@"C:\src\Pictures\forrest.png");
+            //Bitmap initialPicture = (Bitmap)Image.FromFile(@"C:\src\Pictures\forrest.png");
+            /*StreamReader streamReader = new System.IO.StreamReader(@"C:\src\Pictures\forrest.png");
+            Bitmap initialPicture = (Bitmap)Bitmap.FromStream(streamReader.BaseStream);
+            streamReader.Close();*/
+
+            const string original = @"C:\src\Pictures\forrest.png";
+            const string swapped = @"C:\src\Pictures\forrest_none_kirsch.png";
+
+            Bitmap initialPicture = new Bitmap(original);
+
+            string resultPictureAfterFilter_ref;
+            string resultSavedBitmap_ref;
+
+            //Act
+            Bitmap resultPictureAfterFilter = initialPicture.KirschFilter();
+            Bitmap resultSavedBitmap = new Bitmap(swapped);
+
+            //Assert
+            if (resultPictureAfterFilter.Width == resultSavedBitmap.Width && resultPictureAfterFilter.Height == resultSavedBitmap.Height)
+            {
+                for (int i = 0; i < resultPictureAfterFilter.Width; i++)
+                {
+                    for (int j = 0; j < resultPictureAfterFilter.Height; j++)
+                    {
+                        resultPictureAfterFilter_ref = resultPictureAfterFilter.GetPixel(i, j).ToString();
+                        resultSavedBitmap_ref = resultSavedBitmap.GetPixel(i, j).ToString();
+
+                        Assert.AreEqual(resultPictureAfterFilter_ref, resultSavedBitmap_ref);
+                    }
+                }
+            }
+
         }
         [TestMethod]
         public void Sobel3x3EdgeDetectionTest()
